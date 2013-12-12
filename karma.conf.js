324c3631eccdb061ad80a159d313435afbec6d63
x@ES <deal@coolcab.org>
2013-12-12 10:31:02 +0400

    fix karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -63,7 +63,7 @@ module.exports = function(config) {
     // - PhantomJS
     // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
     // browsers: ['Chrome'],
-    browsers: ['Firefox'],
+    // browsers: ['Firefox'],
 
 
     // If browser does not capture in given timeout [ms], kill it
